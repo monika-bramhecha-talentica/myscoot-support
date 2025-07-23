d83f2cc1d31985468de4e9a088156796ebc36fda
monika-bramhecha-talentica <monikab@talentica.com>
2025-07-23 18:19:35 +0530

    Update firebase.ts config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,23 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDxxx", // Replace with your Firebase config
+//   authDomain: "your-project.firebaseapp.com",
+//   projectId: "your-project-id",
+//   storageBucket: "your-project.appspot.com",
+//   messagingSenderId: "123456789",
+//   appId: "1:123456789:web:abcdef"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDxxx", // Replace with your Firebase config
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyD0LR2cS3ee562tPtC006KHYrev3BLV5EM",
+  authDomain: "myscoot-application.firebaseapp.com",
+  projectId: "myscoot-application",
+  storageBucket: "myscoot-application.firebasestorage.app",
+  messagingSenderId: "497438232417",
+  appId: "1:497438232417:web:ae92e76d27481d22043f20",
+  measurementId: "G-8TQ4XL6VLF"
 };
 
 // Initialize Firebase
